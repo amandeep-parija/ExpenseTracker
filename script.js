8d760f1c6f8ca77d458d934913b1806477e2f65f
amandeep-parija <93464533+amandeep-parija@users.noreply.github.com>
2026-02-26 17:13:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,76 @@
+let taskbtn = document.querySelector(".task-btn");
+let form = document.querySelector("#form");
+let modal = document.querySelector(".modal");
+let bgremove = document.querySelector(".bg");
+let addtskbtn = document.querySelector(".add-btn");
+let titleInput  = document.querySelector("#task-title-input");
+let titleAmount  = document.querySelector("#task-amnt-input");
+let categorySelect = document.querySelector("select");
+let table = document.querySelector("table")
+
+
+function newDiv(){
+
+    let newRow = document.createElement("tr");
+
+    let cell1 = document.createElement("td");
+    let cell2 = document.createElement("td");
+    let cell3 = document.createElement("td");
+    let cell4 = document.createElement("td");
+
+    let cell5 = document.createElement("td");
+
+    let actionButton = document.createElement("button")
+    actionButton.innerText = "Delete";
+    actionButton.className = "del-btn";
+    actionButton.onclick = function(){
+        alert("Button in the new row clicked!");
+    };
+ 
+
+    cell1.textContent = titleInput.value.trim();
+    cell2.textContent = titleAmount.value.trim();
+    cell3.textContent = categorySelect.value;
+    cell4.textContent = titleAmount.value.trim();
+
+    cell5.appendChild(actionButton);
+
+
+    newRow.appendChild(cell1);
+    newRow.appendChild(cell2);
+    newRow.appendChild(cell3);
+    newRow.appendChild(cell4);
+    newRow.appendChild(cell5);
+
+
+    table.appendChild(newRow)
+}
+
+
+taskbtn.addEventListener("click", function(){
+    modal.classList.add("active");
+
+    form.reset();
+})
+
+bgremove.addEventListener("click", function(){
+    modal.classList.remove("active");
+})
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    
+    let input = titleInput.value.trim();
+    let amount = titleAmount.value.trim();
+    let select = categorySelect.value;
+
+    
+    console.log(input, amount, select)
+    modal.classList.remove("active");
+
+
+    newDiv()
+    
+})
+
+
